58299b6aa2e6b61f8521da592167a18a93862ca9
mait <mait@fenomen.ee>
2015-12-05 18:00:10 +0200

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DOCKER_COMPOSE_FILE="/var/www/"${1}"/docker-compose.yml"
-PROJECT_ROOT="/var/www/"${1}
+DOCKER_COMPOSE_FILE="/var/www/${1}/docker-compose.yml"
+PROJECT_ROOT="/var/www/${1}"
 
 php_container_exists() {
   echo "$(cd ${PROJECT_ROOT} && docker-compose -f ${DOCKER_COMPOSE_FILE} ps php 2> /dev/null | grep _php_ | awk '{ print $1 }')"
@@ -16,12 +16,10 @@ php_container_running() {
 if [ -z ${1} ] ; then
     echo 'Enter project to start'
 else
-    if [ -f "$DOCKER_COMPOSE_FILE" ]; then
-
+    if [ -f "${DOCKER_COMPOSE_FILE}" ]; then
         CONTAINER="$(php_container_exists)"
 
         if [ -n "$(php_container_running ${CONTAINER})" ]; then
-
             echo "Project already running!"
             read -p "Would you like to stop the containers? [Y/n]: " ANSWER
 
@@ -34,6 +32,6 @@ else
             docker-compose -f "${DOCKER_COMPOSE_FILE}" up -d
         fi
     else
-        echo "folder" ${1} "does not exits or does not contain docker-compose.yml file"
+        echo "folder ${1} does not exits or does not contain docker-compose.yml file"
     fi
 fi
